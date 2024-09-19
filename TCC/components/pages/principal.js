4277973235060553faec5e844669adad99ce322a
marcos-aurelio-etec <marcosllima.marcia@gmail.com>
2024-09-19 23:44:15 +0000

    save19-09

--- TCC/components/pages/principal.js
+++ TCC/components/pages/principal.js
@@ -12,66 +12,74 @@ export default function Principal() {
                 <ScrollView contentContainerStyle={styles.scrollContainer}>
                     <View style={styles.header}>
                         <Image
-                            source={{ uri: 'https://picsum.photos/200/300' }}
+                            source={{ uri: 'https://images.pexels.com/photos/2040189/pexels-photo-2040189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }}
                             style={styles.headerImage}
                         />
                         <Text style={styles.headerText}>Barbearia Franco</Text>
                     </View>
 
                     <View style={styles.container}>
-                       
-                                <View style={styles.content} />
-                                <View style={styles.fotos}>
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/300' }}
-                                        style={styles.fotoImage}
-                                    />
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/301' }}
-                                        style={styles.fotoImage}
-                                    />
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/302' }}
-                                        style={styles.fotoImage}
-                                    />
-                                </View>
-                                <TouchableOpacity style={styles.button} onPress={() => setPage('barbearia1')}>
-                                    <Text style={styles.buttonText}>Entrar</Text>
-                                </TouchableOpacity>
 
-                                <View style={styles.header}>
+                        <View style={styles.content} />
+                        <View style={styles.fotos}>
+                            <Image
+                                source={{ uri: 'https://images.pexels.com/photos/4625631/pexels-photo-4625631.jpeg?auto=compress&cs=tinysrgb&w=600' }}
+                                style={styles.fotoImage}
+                            />
+                            <Image
+                                source={{ uri: 'https://images.pexels.com/photos/4625624/pexels-photo-4625624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }}
+                                style={styles.fotoImage}
+                            />
+                            <Image
+                                source={{ uri: 'https://images.pexels.com/photos/3998429/pexels-photo-3998429.jpeg?auto=compress&cs=tinysrgb&w=600' }}
+                                style={styles.fotoImage}
+                            />
+                        </View>
+                        <TouchableOpacity style={styles.button} onPress={() => setPage('barbearia1')}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </TouchableOpacity>
+
+                        
+                        <Text style={styles.text}>
+                            {'\n'}
+                          {'\n'}
+                            
+                            
+                        </Text>
+
+                    </View>
+                    <View style={styles.header}>
                         <Image
                             source={{ uri: 'https://picsum.photos/200/300' }}
                             style={styles.headerImage}
                         />
-                        <Text style={styles.headerText}>Barbearia HenriqueDuCorte</Text>
+                        <Text style={styles.headerText}>Barbearia DoHenrique</Text>
                     </View>
 
-                    <View style={styles.fotos}>
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/300' }}
-                                        style={styles.fotoImage}
-                                    />
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/301' }}
-                                        style={styles.fotoImage}
-                                    />
-                                    <Image
-                                        source={{ uri: 'https://picsum.photos/200/302' }}
-                                        style={styles.fotoImage}
-                                    />
-                                </View>
+                    <View style={styles.container}>
 
-                    
-                                
-                                
+                        <View style={styles.content} />
+                        <View style={styles.fotos}>
+                            <Image
+                                source={{ uri: 'https://picsum.photos/200/300' }}
+                                style={styles.fotoImage}
+                            />
+                            <Image
+                                source={{ uri: 'https://picsum.photos/200/301' }}
+                                style={styles.fotoImage}
+                            />
+                            <Image
+                                source={{ uri: 'https://picsum.photos/200/302' }}
+                                style={styles.fotoImage}
+                            />
+                        </View>
+                        <TouchableOpacity style={styles.button} onPress={() => setPage('barbearia1')}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </TouchableOpacity>
 
-                                
-                                
-                               
-                            </View>
-                       
-                    
+
+                    </View>
+
                 </ScrollView>
             );
         } else if (page === 'home') {
@@ -110,6 +118,7 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         borderRadius: 50,
+        color: 'black',
     },
     headerText: {
         fontSize: 18,
@@ -134,7 +143,10 @@ const styles = StyleSheet.create({
     },
     fotoImage: {
         width: 100,
-        height: 100,
+        height: 200,
+        marginLeft: 30,
+        borderRadius: 50,
+
     },
     button: {
         backgroundColor: '#003818',
@@ -148,5 +160,5 @@ const styles = StyleSheet.create({
     },
     boxSpacing: {
         marginTop: 20, // Adiciona 20 unidades de espaçamento acima
-      },
+    },
 });
